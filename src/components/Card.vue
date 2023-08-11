a6d884eb17cf73ef9416fabc4cd06bdfed0a4a80
鄭翊宏 <>
2023-08-11 16:57:30 +0800

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4 w-full md:w-1/2">
+    <div class="card bg-base-300 shadow-xl box-content">
+      <figure class="w-full h-48">
+        <slot name="WorkImg"> </slot>
+      </figure>
+      <div class="card-body gap-0">
+        <h2 class="card-title">
+          <slot name="title"></slot>
+        </h2>
+        <p><slot name="info"></slot></p>
+        <p><slot name="content"></slot></p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/CodeMockup.vue
+++ src/components/CodeMockup.vue
@@ -2,11 +2,11 @@
 const prop = defineProps(["code"]);
 </script>
 <template>
-  <div class="mockup-code w-full">
+  <div class="mockup-code w-full bg-slate-500">
     <slot name="content">
       <pre
         data-prefix="$"
-        class="text-secondary"
+        class="text-emerald-400"
       ><code>{{prop.code}}</code></pre>
     </slot>
   </div>
